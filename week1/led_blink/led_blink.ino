eb337cbb4675c3aad32d5ab9192aa96c46991d6b
lakshaysh36-hue <lakshaysh36@gmail.com>
2026-07-04 00:27:34 +0530

    feat: add serial blink counter

--- week1/led_blink/led_blink.ino
+++ week1/led_blink/led_blink.ino
@@ -1,5 +1,8 @@
+int blinkCount = 0;
+
 void setup() {
   pinMode(13, OUTPUT);
+  Serial.begin(9600);
 }
 
 void loop() {
@@ -8,4 +11,8 @@ void loop() {
 
   digitalWrite(13, LOW);
   delay(500);
+
+  blinkCount++;
+  Serial.print("Blink count: ");
+  Serial.println(blinkCount);
 }
