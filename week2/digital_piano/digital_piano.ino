1508cf890834486b25b37b54607a657597bdec74
lakshaysh36-hue <lakshaysh36@gmail.com>
2026-07-05 14:58:13 +0530

    feat: add basic digital piano with four notes

---
+++ week2/digital_piano/digital_piano.ino
@@ -0,0 +1,43 @@
+/*
+Project: Digital Piano
+Author: Lakshay Sharma
+Description:
+Basic digital piano with four notes.
+*/
+
+const int buzzerPin = 8;
+
+const int doButton = 2;
+const int reButton = 3;
+const int miButton = 4;
+const int faButton = 5;
+
+void setup()
+{
+    pinMode(doButton, INPUT_PULLUP);
+    pinMode(reButton, INPUT_PULLUP);
+    pinMode(miButton, INPUT_PULLUP);
+    pinMode(faButton, INPUT_PULLUP);
+
+    pinMode(buzzerPin, OUTPUT);
+}
+
+void loop()
+{
+    if (digitalRead(doButton) == LOW)
+    {
+        tone(buzzerPin, 262);
+    }
+    else if (digitalRead(reButton) == LOW)
+    {
+        tone(buzzerPin, 294);
+    }
+    else if (digitalRead(miButton) == LOW)
+    {
+        tone(buzzerPin, 330);
+    }
+    else if (digitalRead(faButton) == LOW)
+    {
+        tone(buzzerPin, 349);
+    }
+}
